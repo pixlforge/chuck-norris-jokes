d19f570ae0416b06dcced11795baa4389f03d6cf
pixlforge <celien@pixlforge.ch>
2019-02-24 18:52:14 +0000

    Added facades

---
+++ src/Facades/ChuckNorris.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pixlforge\ChuckNorrisJokes;
+
+use Illuminate\Support\Facades\Facade;
+
+class ChuckNorris extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'chuck-norris';
+    }
+}
