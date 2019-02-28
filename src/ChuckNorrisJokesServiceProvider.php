9bf3e3f3f6e3829640e651074678b997579cd174
pixlforge <celien@pixlforge.ch>
2019-02-28 10:34:13 +0000

    Added support for console command

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -3,12 +3,17 @@
 namespace Pixlforge\ChuckNorrisJokes;
 
 use Illuminate\Support\ServiceProvider;
+use Pixlforge\ChuckNorrisJokes\Console\ChuckNorrisJoke;
 
 class ChuckNorrisJokesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        //
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ChuckNorrisJoke::class,
+            ]);
+        }
     }
 
     public function register()
---
+++ src/Console/ChuckNorrisJoke.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pixlforge\ChuckNorrisJokes\Console;
+
+use Illuminate\Console\Command;
+use Pixlforge\ChuckNorrisJokes\Facades\ChuckNorris;
+
+class ChuckNorrisJoke extends Command
+{
+    protected $signature = 'chuck-norris';
+
+    protected $description = 'Output a funny Chuck Norris joke.';
+
+    public function handle()
+    {
+        $this->info(ChuckNorris::getRandomJoke());
+    }
+}
