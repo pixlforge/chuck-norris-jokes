27edef34ac607c045f4ce979649ec500153b6768
pixlforge <celien@pixlforge.ch>
2019-02-28 11:08:01 +0000

    Added console command test

---
+++ tests/LaravelTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Pixlforge\ChuckNorrisJokes\Tests;
+
+use Orchestra\Testbench\TestCase;
+use Illuminate\Support\Facades\Artisan;
+use Pixlforge\ChuckNorrisJokes\Facades\ChuckNorris;
+use Pixlforge\ChuckNorrisJokes\ChuckNorrisJokesServiceProvider;
+
+class LaravelTest extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            ChuckNorrisJokesServiceProvider::class,
+        ];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [
+            'ChuckNorris' => ChuckNorris::class,
+        ];
+    }
+
+    /** @test */
+    public function the_console_command_returns_a_joke()
+    {
+        $this->withoutMockingConsoleOutput();
+
+        ChuckNorris::shouldReceive('getRandomJoke')
+            ->once()
+            ->andReturn('some joke');
+
+        $this->artisan('chuck-norris');
+
+        $output = Artisan::output();
+
+        $this->assertSame('some joke' . PHP_EOL, $output);
+    }
+}
